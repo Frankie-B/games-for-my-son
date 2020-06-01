7b58923b00d670855a21178c977c673f93bab8bd
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-01 09:48:07 +0200

    Add countdown timer functionality

--- whack-a-mole/index.js
+++ whack-a-mole/index.js
@@ -3,6 +3,7 @@ const pig = document.querySelectorAll('.pig');
 const timeLeft = document.querySelector('#time-left');
 let score = document.querySelector('#score');
 let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
   square.forEach((className) => {
@@ -30,4 +31,15 @@ function movePig() {
   timerId = setInterval(randomSquare, 1000);
 }
 
+function countDownTimer() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime === 0) {
+    clearInterval(timerId);
+    alert(`GAME OVER! Your final score is ${result}`);
+  }
+}
+
+let timerId = setInterval(countDownTimer, 1000);
 // movePig();
